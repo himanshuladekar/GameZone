374b970ef7d1833851b9eee2d23de1c9b2579bba
Himanshu Ladekar <himanshuladekar21@gmail.com>
2025-03-16 11:38:13 +0530

    comp changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://himanshuladekar21:Hima@123@cluster0.qcu4s.mongodb.net/games-site', {
-      
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected');
+    await mongoose.connect('mongodb+srv://himanshuladekar21:Hima@123@cluster0.qcu4s.mongodb.net/games-site');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
