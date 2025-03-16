94542bf9a83dbbbfb0ff494cce9bba46844f1090
Himanshu Ladekar <himanshuladekar21@gmail.com>
2025-03-16 11:25:20 +0530

    completed chages

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb+srv://himanshuladekar21:Hima@123@cluster0.qcu4s.mongodb.net/games-site', {
-      useNewUrlParser: true,
+      
       useUnifiedTopology: true,
     });
     console.log('MongoDB Connected');
