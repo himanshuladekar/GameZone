46bd36d98b1894d93428facf2fa1ab94ef421547
Himanshu Ladekar <himanshuladekar21@gmail.com>
2025-03-16 11:08:23 +0530

    updated config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/games-site', {
+    await mongoose.connect('mongodb+srv://himanshuladekar21:Hima@123@cluster0.qcu4s.mongodb.net/games-site', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
